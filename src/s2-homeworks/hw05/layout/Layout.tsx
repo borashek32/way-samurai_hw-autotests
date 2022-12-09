8bcfe9098166f7a5592462d0d1427879f16f5184
borashek <borashek@inbox.ru>
2022-12-09 15:19:19 +0300

    without styles

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -1,29 +1,34 @@
-import React, { FC, ReactNode, useEffect, useState } from 'react'
-import { Header } from '../header/Header'
-import { Sidebar } from '../sidebar/Sidebar'
+import React, {FC, ReactNode, useEffect, useState} from 'react'
+import {Header} from '../header/Header'
+import {Sidebar} from '../sidebar/Sidebar'
+import s from '../../../s1-main/App.module.css'
 
 type PropsType = {
-    children: ReactNode
+  children: ReactNode
 }
 
-export const Layout: FC<PropsType> = ({ children }) => {
-    const [open, setOpen] = useState(false)
-    const handleClose = () => setOpen(false)
-    const handleOpen = () => setOpen(true)
+export const Layout: FC<PropsType> = ({children}) => {
+  const [open, setOpen] = useState(false)
+  const handleClose = () => setOpen(false)
+  const handleOpen = () => setOpen(true)
 
-    useEffect(() => {
-        open && (document.body.style.overflow = 'hidden')
-        !open && (document.body.style.overflow = 'unset')
-    }, [open]) // отключает прокрутку при открытом меню
+  useEffect(() => {
+    open && (document.body.style.overflow = 'hidden')
+    !open && (document.body.style.overflow = 'unset')
+  }, [open]) // отключает прокрутку при открытом меню
 
-    return (
-        <>
-            <Sidebar open={open} handleClose={handleClose} />
-            <Header handleOpen={handleOpen} />
-            <div>
-                {/*страницы*/}
-                {children}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Sidebar open={open} handleClose={handleClose} />
+      <div>
+        <Header handleOpen={handleOpen}/>
+        <div className={s.hw}>
+          <div>
+            {/*страницы*/}
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
