e6a2f7c6ff2c9966b35619531579739855973d1c
borashek <borashek@inbox.ru>
2023-01-28 17:00:37 +0300

    11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -26,7 +26,7 @@ function HW11() {
   };
 
   return (
-    <div id={"hw10"}>
+    <div id={"hw11"}>
       <div className={s2.container} style={{ paddingTop: "40px" }}>
         <div className={s2.hwTitle}>Homework #11</div>
       </div>
