d4f83e81f6fb31af8163df43e90b579ade9948a0
borashek <borashek@inbox.ru>
2023-03-14 22:24:13 +0300

    opposite sort

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -18,10 +18,10 @@ export type SuperSortPropsType = {
 
 export const pureChange = (sort: string, down: string, up: string) => {
   // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-  if (sort === '') return down
-  if (sort === down) return up
-  if (sort === up) return '' // исправить
-  else return down
+  if (sort === '') return up
+  if (sort === up) return down
+  if (sort === down) return '' // исправить
+  else return up
 }
 
 
